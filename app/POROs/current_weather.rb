73fe8b5d37d9b486cdba0a65a667f4a193218a08
Jordan Beck <jordanfbeck@gmail.com>
2021-04-24 12:16:24 -0600

    Add more testing for forecast request

--- app/POROs/current_weather.rb
+++ app/POROs/current_weather.rb
@@ -3,9 +3,15 @@ class CurrentWeather
   attr_reader :datetime, :sunrise, :sunset, :temperature, :feels_like, :humidity, :uvi, :visibility, :conditions, :icon
 
   def initialize(current_data, timezone_offset)
-    # require "pry"; binding.pry
-    # @datetime = Time.at(current_data[0][:dt]).to_time.to_s
-    # require "pry"; binding.pry
     @datetime = Time.at(current_data[:dt]).getlocal(timezone_offset).to_s
+    @sunrise = Time.at(current_data[:sunrise]).getlocal(timezone_offset).to_s
+    @sunset = Time.at(current_data[:sunset]).getlocal(timezone_offset).to_s
+    @temperature = current_data[:temp]
+    @feels_like = current_data[:feels_like]
+    @humidity = current_data[:humidity]
+    @uvi = current_data[:uvi]
+    @visibility = current_data[:visibility]
+    @conditions = current_data[:weather][0][:description]
+    @icon = current_data[:weather][0][:icon]
   end
 end
--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -25,9 +25,11 @@ RSpec.describe 'Forecast API request', type: :request do
     expect(forecast[:data]).to be_a(Hash)
     expect(forecast[:data]).to have_key(:id)
     expect(forecast[:data]).to have_key(:type)
+    require "pry"; binding.pry
     expect(forecast[:data]).to have_key(:attributes)
     expect(forecast[:data][:id]).to be_a(String)
     expect(forecast[:data][:type]).to be_a(String)
+    expect(forecast[:data][:type]).to eq("forecast")
     expect(forecast[:data][:attributes]).to be_a(Hash)
     expect(forecast[:data][:attributes]).to have_key(:current_weather)
     expect(forecast[:data][:attributes]).to have_key(:hourly_weather)
@@ -41,6 +43,26 @@ RSpec.describe 'Forecast API request', type: :request do
     expect(forecast[:data][:attributes][:daily_weather]).to be_a(Hash)
     expect(forecast[:data][:attributes][:daily_weather]).to have_key(:date)
     expect(forecast[:data][:attributes][:daily_weather][:date]).to be_a(String)
+    expect(forecast[:data][:attributes][:current_weather].count).to eq(10)
+    expect(forecast[:data][:attributes][:current_weather]).to have_key(:sunrise)
+    expect(forecast[:data][:attributes][:current_weather]).to have_key(:sunset)
+    expect(forecast[:data][:attributes][:current_weather]).to have_key(:temperature)
+    expect(forecast[:data][:attributes][:current_weather]).to have_key(:feels_like)
+    expect(forecast[:data][:attributes][:current_weather]).to have_key(:humidity)
+    expect(forecast[:data][:attributes][:current_weather]).to have_key(:uvi)
+    expect(forecast[:data][:attributes][:current_weather]).to have_key(:visibility)
+    expect(forecast[:data][:attributes][:current_weather]).to have_key(:conditions)
+    expect(forecast[:data][:attributes][:current_weather]).to have_key(:icon)
+    expect(forecast[:data][:attributes][:current_weather][:sunrise]).to be_a(String)
+    expect(forecast[:data][:attributes][:current_weather][:sunset]).to be_a(String)
+    expect(forecast[:data][:attributes][:current_weather][:temperature]).to be_a(Float)
+    expect(forecast[:data][:attributes][:current_weather][:feels_like]).to be_a(Float)
+    expect(forecast[:data][:attributes][:current_weather][:humidity]).to be_a(Integer)
+    expect(forecast[:data][:attributes][:current_weather][:uvi]).to be_a(Float)
+    expect(forecast[:data][:attributes][:current_weather][:visibility]).to be_a(Integer)
+    expect(forecast[:data][:attributes][:current_weather][:conditions]).to be_a(String)
+    expect(forecast[:data][:attributes][:current_weather][:icon]).to be_a(String)
+    # require "pry"; binding.pry
 
     # a data attribute, under which all other attributes are present:
 # id, always set to null
