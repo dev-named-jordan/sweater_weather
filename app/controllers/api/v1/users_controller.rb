be1f6708be2db23a165b2cfdddc18d4c80d4e866
Jordan Beck <jordanfbeck@gmail.com>
2021-04-25 13:22:18 -0600

    Add code for user request, and securerandom gem

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    @user = User.new(email: params[:email].downcase, password: params[:password], password_confirmation: params[:password_confirmation], api_key: SercureRandom.hex)
+    # require "pry"; binding.pry
+    user = User.new(email: params[:email].downcase, password: params[:password], password_confirmation: params[:password_confirmation], api_key: SecureRandom.hex)
     if user.save
       serial = UserSerializer.new(user)
-      render json: @serial, status: 201
+      render json: serial, status: 201
     else
-      render json: not sure
+      render json: {'Message': {'Error': "#{user.errors.full_messages}" }}, status: :bad_request
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :email, presence: true
   validates :password, confirmation: true
-  validates :password, require: true
   validates :api_key, presence: true
 
   has_secure_password
