1a6912b8a4dc635532fd3de00e088fa84b29e253
Jordan Beck <jordanfbeck@gmail.com>
2021-04-25 13:21:34 -0600

    Add sad path testing for users request

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'User request', type: :request do
     User.destroy_all
 
     params = {
-      "email": "email@example.com",
+      "email": "email_1@example.com",
       "password": "1234",
       "password_confirmation": "1234"
     }
@@ -27,5 +27,50 @@ RSpec.describe 'User request', type: :request do
     expect(expected[:data][:attributes].count).to eq(2)
     expect(expected[:data][:attributes][:email]).to be_a(String)
     expect(expected[:data][:attributes][:api_key]).to be_a(String)
+
+    post api_v1_users_path, headers: headers, params: JSON.generate(params)
+
+    expected = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+  end
+  describe 'sad path' do
+    it 'gives error message for missing field' do
+      User.destroy_all
+
+      params = {
+        "email": "email_2@example.com",
+        "password": "",
+        "password_confirmation": ""
+      }
+
+      headers = { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+
+      post api_v1_users_path, headers: headers, params: JSON.generate(params)
+
+      expected = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+    end
+    it 'gives error message for missing field' do
+      User.destroy_all
+
+      params = {
+        "email": "email_3@example.com",
+        "password": "1234",
+        "password_confirmation": "4321"
+      }
+
+      headers = { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+
+      post api_v1_users_path, headers: headers, params: JSON.generate(params)
+
+      expected = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+    end
   end
 end
