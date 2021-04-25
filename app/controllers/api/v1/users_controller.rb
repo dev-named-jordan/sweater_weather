53557cdd020fb452b88c23704211c3adda7c0737
Jordan Beck <jordanfbeck@gmail.com>
2021-04-25 11:41:10 -0600

    Add user model spec and code

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   def create
     require "pry"; binding.pry
+    @user = User.new
   end
 end
--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'User request', type: :request do
   it 'Can create new user', :vcr do
 
-    POST "/api/v1/users"
+    post "/api/v1/users"
 
     picture = JSON.parse(response.body, symbolize_names:true)
 
