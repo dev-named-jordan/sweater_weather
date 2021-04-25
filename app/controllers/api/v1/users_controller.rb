9f6273881e1edca688d62940f9e3004f4912f238
Jordan Beck <jordanfbeck@gmail.com>
2021-04-25 12:01:20 -0600

    Add users table to database

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    require "pry"; binding.pry
-    @user = User.new
+    @user = User.new(email: params[:email].downcase, password: params[:password], password_confirmation: params[:password_confirmation], api_key: SercureRandom.hex)
+    if user.save
+      serial = UserSerializer.new(user)
+      render json: @serial, status: 201
+    else
+      render json: not sure
+    end
   end
 end
--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'User request', type: :request do
   it 'Can create new user', :vcr do
 
     post "/api/v1/users"
-
+require "pry"; binding.pry
     picture = JSON.parse(response.body, symbolize_names:true)
 
     expect(response).to be_successful
