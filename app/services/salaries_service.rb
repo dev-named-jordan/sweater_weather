a8406e2e2078d8080e5708620e8bef65bf7a7dab
Jordan Beck <jordanfbeck@gmail.com>
2021-04-26 11:14:13 -0600

    Add get destination url service call

--- app/services/salaries_service.rb
+++ app/services/salaries_service.rb
@@ -1,16 +1,24 @@
 class SalariesService
-      def self.conn
-        Faraday.new(url: 'https://api.teleport.org')
-      end
+  def self.conn
+    Faraday.new(url: 'https://api.teleport.org')
+  end
 
-      def self.parse(response)
-        JSON.parse(response.body, symbolize_names: true)
-      end
+  def self.parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
-      def self.get_destination(destination)
-        response = conn.get("/api/cities/?search=#{destination}") do |f|
-          f.params['search'] = destination
-        end
-        parse(response)
-      end
+  def self.get_destination(destination)
+    response = conn.get("/api/cities/?search=#{destination}") do |f|
+      f.params['search'] = destination
     end
+    parse(response)
+  end
+
+  def self.get_destination_url(destination)
+    response = conn.get("/api/cities/?search=#{destination}") do |f|
+      f.params['search'] = destination
+    end
+    parse(response)[:_embedded][:"city:search-results"][0][:_links][:"city:item"][:href]
+  end
+  
+end
