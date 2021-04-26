dd23e1265e6d11592fa7e7ebcfc9b96a554bf701
Jordan Beck <jordanfbeck@gmail.com>
2021-04-26 11:15:20 -0600

    Update facade

--- app/facades/salaries_facade.rb
+++ app/facades/salaries_facade.rb
@@ -1,6 +1,9 @@
 class SalariesFacade
     def self.get_destination(destination)
-      response = SalariesService.get_destination(destination)
-      Salaries.new(response)
+      salaries_response = SalariesService.get_destination(destination)
+      require "pry"; binding.pry
+      destination_url_response = SalariesService.get_destination_url(destination)
+      forecast_response = ForecastFacade.get_forecast(destination)
+      Salaries.new(salaries_response, forecast_response)
     end
   end
