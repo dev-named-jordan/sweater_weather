b28c168ff2f6a94dad455f97936f4785d49edaf5
Jordan Beck <jordanfbeck@gmail.com>
2021-04-26 09:10:38 -0600

    Add test for teleport request spec

---
+++ spec/requests/api/v1/teleport_request_spec.rb
@@ -0,0 +1,20 @@
+teleport request spec
+
+    require 'rails_helper'
+
+    RSpec.describe 'Teleport API request', type: :request do
+      before :each do
+        c.allow_http_connections_when_no_cassette = true
+      end
+      it 'Can search for a city by name' do
+
+        get "/api/v1/salaries?destination=denver,co"
+
+        destination = JSON.parse(response.body, symbolize_names:true)
+
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+        expect(destination).to be_a(Hash)
+        expect(destination).to have_key(:data)
+      end
+    end
