0125f07cfee4ef5dd4a69d8f98674f34231fa102
Jordan Beck <jordanfbeck@gmail.com>
2021-04-26 16:42:46 -0600

    Moving around tests

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-
+    # require "pry"; binding.pry
+    # user = User.find_by(email: params[:email].downcase)
+    # if user && user.authenticate(params[:password])
+    #   serial = UserSerializer.new(user)
+    #   render json: serial, status: 200
+    # else
+    #   render json: {'Message': {'Error': "Credentials are bad, and you should feel bad" }}, status: :bad_request
+    # end
   end
 end
--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -6,8 +6,6 @@ RSpec.describe 'Forecast API request', type: :request do
 
     user = User.create!(email: "email_1@example.com", password: "1234", password_confirmation: "1234", api_key: SecureRandom.hex)
 
-    post  "/api/v1/road_trip"
-
     body = {
       "origin": "Denver,CO",
       "destination": "Pueblo,CO",
@@ -16,11 +14,11 @@ RSpec.describe 'Forecast API request', type: :request do
 
     headers = { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
 
-    post api_v1_sessions_path, headers: headers, params: JSON.generate(user_data)
+    post "/api/v1/road_trip", headers: headers, params: JSON.generate(body)
 
+    require "pry"; binding.pry
     expected = JSON.parse(response.body, symbolize_names: true)
 
-# require "pry"; binding.pry
     expect(response).to be_successful
     expect(response.status).to eq(200)
     expect(expected).to be_a(Hash)
