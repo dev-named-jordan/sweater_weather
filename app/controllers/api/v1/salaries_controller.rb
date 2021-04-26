c8d4cff81f81c914f921d49b5806260582e25d50
Jordan Beck <jordanfbeck@gmail.com>
2021-04-26 10:00:51 -0600

    Adds controller

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::SalariesController < ApplicationController
   def index
-    location = params[:location]
-    if location.blank?
+    destination = params[:destination]
+    if destination.blank?
       render json: {'Message': {'Error': 'Invalid Info Provided'}}, status: :bad_request
     else
-      response = SalaryFacade.get_teleport(location)
-      render json: SalarySerializer.new(response)
+      response = SalariesFacade.get_destination(destination)
+      render json: SalariesSerializer.new(response)
     end
   end
 end
