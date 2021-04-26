a38e09464f4bbbb98a5d21c9cedf3393760037ce
Jordan Beck <jordanfbeck@gmail.com>
2021-04-26 11:24:47 -0600

    Add poro changes

--- app/POROs/salaries.rb
+++ app/POROs/salaries.rb
@@ -1,11 +1,11 @@
 class Salaries
 
-attr_reader :destination_url, :destination_forecast, :destination
+attr_reader :destination_forecast, :destination
 
   def initialize(salaries_response, forecast_response)
-    require "pry"; binding.pry
-    @destinaiton = ig
-    @destination_url = salaries_response[:_embedded][:"city:search-results"][0][:_links][:"city:item"][:href]
-    @destination_forecast = forecast_response.current_weather.conditions
+    @destination = salaries_response[:_embedded][:"city:search-results"][0][:matching_full_name]
+    @destination_forecast = { summary: forecast_response.current_weather.conditions, temperature: forecast_response.current_weather.temperature }
   end
+
+  
 end
