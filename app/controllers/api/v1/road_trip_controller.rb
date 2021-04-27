08e2b6656da23664d03054b85f2535fb04f6cbc5
Jordan Beck <jordanfbeck@gmail.com>
2021-04-26 18:11:37 -0600

    Add findby for user in create action for roadtrip controller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    # require "pry"; binding.pry
+    u = User.find_by(api_key: params[:road_trip][:origin] && params[:road_trip][:destination] &&  params[:road_trip][:api_key])
+    require "pry"; binding.pry
     # user = User.find_by(email: params[:email].downcase)
     # if user && user.authenticate(params[:password])
     #   serial = UserSerializer.new(user)
